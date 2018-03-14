9c1b49d6e4c75b9b81d8ad70b752d745c7da5112
Jens Walter <js.walter@gmx.net>
2018-03-14 23:12:23 +0100

    initial put_object support

---
+++ MESHIFY_s3_putobject/main.py
@@ -0,0 +1,25 @@
+import boto3
+import json
+import base64
+
+def handler(event, context):
+    bucketName = event['bucketName'];
+    objectName = event['objectName'];
+    if 'contentText' in event:
+        content = event['contentText'].encode("utf-8")
+    else:
+        content = base64.b64decode(event['contentBase64'])
+
+    accessKey = event['account']['accessKey']
+    secretKey = event['account']['secretKey']
+    S3 = boto3.client('s3',
+        aws_access_key_id=accessKey,
+        aws_secret_access_key=secretKey
+    )
+
+    result = S3.put_object(
+        Body=content,
+        Bucket=bucketName,
+        Key=objectName
+    )
+    return result
