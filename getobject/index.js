fec3bd3b9b1e759db468787c19bca58f402ae19d
Jens Walter <jens@apimeister.com>
2019-04-17 09:46:10 +0000

    missing credential for get object

--- getobject/index.js
+++ getobject/index.js
@@ -1,12 +1,12 @@
 const AWS = require("aws-sdk");
-const S3 = new AWS.S3();
 
 exports.handler = async (event, context) => {
-  var params = {
+  const S3 = new AWS.S3({accessKeyId : event.account.accessKey,secretAccessKey : event.account.secretKey });
+  let params = {
     Bucket: event.bucketName, 
     Key: event.objectName
    };
-   let response = S3.getObject(params).promise();
+   let response = await S3.getObject(params).promise();
 
    let result ={
     "contentText": response.Body.toString(),
